cd395e6a424449ec7200924d37a858d701a91b0f
dennistreysa <dennis.pietsch@tu-ilmenau.de>
2015-05-07 20:34:03 +0200

    Committed GUI

---
+++ Softwareprojekt2015/GUI/WorldMap.xaml.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.ComponentModel;
+
+namespace Softwareprojekt2015
+{
+    public partial class WorldMap : INotifyPropertyChanged
+    {
+
+        private static IDictionary<string, int> _mapDictionary = new Dictionary<string, int>();
+        private static IDictionary<string, double> _shadingDictionary = new Dictionary<string, double>();
+        private static IDictionary<string, Path> _pathDictionary = new Dictionary<string, Path>();
+        static SolidColorBrush defaultShading = new SolidColorBrush(Color.FromArgb(255, 204, 204, 204));
+        static SolidColorBrush mouseOverShading = new SolidColorBrush(Colors.Red);
+
+
+        /*
+         *  Events
+         * */
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private string countryName = "-Country_Name-";
+        public string CountryNameString
+        {
+            get
+            {
+                return countryName;
+            }
+            set
+            {
+                countryName = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(App.Current.MainWindow.DataContext, new PropertyChangedEventArgs("CountryNameString"));
+                }
+            }
+
+        }
+
+
+        /*
+         * 
+         * */
+        private void Map_OnLoaded(object sender, EventArgs e)
+        {            
+            foreach (var p in FindVisualChildren<Path>(((MainWindow)System.Windows.Application.Current.MainWindow).mainGrid))
+            {
+                _pathDictionary[cleanCountryCode(p.Name).ToUpper()] = p;
+            }
+        }
+
+
+        /*
+         * 
+         * */
+        private void Map_Country_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var country = sender as Path;
+
+            country.Fill = mouseOverShading;
+            CountryNameString = cleanCountryCode(country.Name);
+        }
+
+
+        /*
+         * 
+         * */
+        private void Map_Country_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var country = sender as Path;
+
+            updateCountry(country.Name);
+            CountryNameString = "";
+        }
+
+
+        /*
+         * 
+         * */
+        public void updateCountry(string countryCode)
+        {
+            countryCode = cleanCountryCode(countryCode).ToUpper();
+
+            if (_shadingDictionary.ContainsKey(countryCode) && _pathDictionary.ContainsKey(countryCode))
+            {
+                SolidColorBrush packageShading = new SolidColorBrush(Colors.Blue);
+
+                packageShading.Opacity = _shadingDictionary[countryCode];
+                _pathDictionary[countryCode].Fill = packageShading;
+            }
+            else
+            {
+                if (_pathDictionary.ContainsKey(countryCode))
+                {
+                    _pathDictionary[countryCode].Fill = defaultShading;
+                }
+            }
+        }
+
+
+        /*
+         * 
+         * */
+        public void setData(string countryCode, int value)
+        {
+            countryCode = cleanCountryCode(countryCode).ToUpper();
+            _mapDictionary[countryCode] = value;
+        }
+
+
+        /*
+         * 
+         * */
+        public void updateCountries()
+        {
+            double minVal = 0.0;
+            double maxVal = 0.0;
+
+            // Get min/max values
+            foreach (var e in _mapDictionary)
+            {
+                if (e.Value < minVal)
+                {
+                    minVal = e.Value;
+                }
+
+                if (e.Value > maxVal)
+                {
+                    maxVal = e.Value;
+                }
+            }
+
+            _shadingDictionary.Clear();
+
+            // Shade countires
+            foreach (var e in _mapDictionary)
+            {
+                double opacity = (e.Value - minVal) / (maxVal - minVal);
+
+                // Minimum is 3%
+                if (opacity < 0.010)
+                {
+                    opacity = 0.010;
+                }
+
+                _shadingDictionary[e.Key] = opacity;
+            }
+
+
+            foreach (var p in FindVisualChildren<Path>(((MainWindow)System.Windows.Application.Current.MainWindow).mainGrid))
+            {
+                string name = cleanCountryCode(p.Name).ToUpper();
+
+                if (_shadingDictionary.ContainsKey(name))
+                {
+                    SolidColorBrush packageShading = new SolidColorBrush(Colors.Blue);
+
+                    packageShading.Opacity = _shadingDictionary[name];
+                    p.Fill = packageShading;
+                }
+            }
+        }
+
+
+        /*
+         * 
+         * */
+        public string cleanCountryCode(string code)
+        {
+            if (code.ToUpper().StartsWith("MAP"))
+            {
+                // Remove 'Map' prefix
+                code = code.Substring(3);
+            }
+            return code.Substring(0, 2);
+        }
+
+
+        /*
+         * 
+         * */
+        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject parent) where T : DependencyObject
+        {
+            List<T> foundChilds = new List<T>();
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+
+                T childType = child as T;
+                if (childType == null)
+                {
+                    foreach (var other in FindVisualChildren<T>(child))
+                        yield return other;
+                }
+                else
+                {
+                    yield return (T)child;
+                }
+            }
+        }
+
+    }
+}
---
+++ Softwareprojekt2015/ViewModel/GUIViewModel.cs
@@ -0,0 +1,478 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Softwareprojekt2015
+{
+    public class GUIViewModel : INotifyPropertyChanged
+    {
+        /*
+         *  Public members
+         * */
+        public DelegateCommand AddSeriesCommand { get; set; }
+        public List<double> FontSizes { get; set; }
+        public List<double> DoughnutInnerRadiusRatios { get; set; }
+        public List<string> SelectionBrushes { get; set; }
+        public ObservableCollection<string> ViewTypes { get; set; }
+        public Dictionary<string, De.TorstenMandelkow.MetroChart.ResourceDictionaryCollection> Palettes { get; set; }
+
+        /*
+         *  selected layout elements
+         * */
+        private string selectedChartType = null;
+        public string SelectedChartType
+        {
+            get
+            {
+                return selectedChartType;
+            }
+            set
+            {
+                selectedChartType = value;
+                NotifyPropertyChanged("SelectedChartType");
+            }
+        }
+
+        private object selectedPalette = null;
+        public object SelectedPalette
+        {
+            get
+            {
+                return selectedPalette;
+            }
+            set
+            {
+                selectedPalette = value;
+                NotifyPropertyChanged("SelectedPalette");
+            }
+        }
+
+        private string selectedBrush = null;
+        public string SelectedBrush
+        {
+            get
+            {
+                return selectedBrush;
+            }
+            set
+            {
+                selectedBrush = value;
+                NotifyPropertyChanged("SelectedBrush");
+            }
+        }
+
+        private double selectedDoughnutInnerRadiusRatio = 0.75;
+        public double SelectedDoughnutInnerRadiusRatio
+        {
+            get
+            {
+                return selectedDoughnutInnerRadiusRatio;
+            }
+            set
+            {
+                selectedDoughnutInnerRadiusRatio = value;
+                NotifyPropertyChanged("SelectedDoughnutInnerRadiusRatio");
+                NotifyPropertyChanged("SelectedDoughnutInnerRadiusRatioString");
+            }
+        }
+
+        private double fontSize = 11.0;
+        public double SelectedFontSize
+        {
+            get
+            {
+                return fontSize;
+            }
+            set
+            {
+                fontSize = value;
+                NotifyPropertyChanged("SelectedFontSize");
+                NotifyPropertyChanged("SelectedFontSizeString");
+            }
+        }
+
+        private bool isRowColumnSwitched = false;
+        public bool IsRowColumnSwitched
+        {
+            get
+            {
+                return isRowColumnSwitched;
+            }
+            set
+            {
+                isRowColumnSwitched = value;
+                NotifyPropertyChanged("IsRowColumnSwitched");
+            }
+        }
+
+        private bool isLegendVisible = true;
+        public bool IsLegendVisible
+        {
+            get
+            {
+                return isLegendVisible;
+            }
+            set
+            {
+                isLegendVisible = value;
+                NotifyPropertyChanged("IsLegendVisible");
+            }
+        }
+
+        private bool isTitleVisible = true;
+        public bool IsTitleVisible
+        {
+            get
+            {
+                return isTitleVisible;
+            }
+            set
+            {
+                isTitleVisible = value;
+                NotifyPropertyChanged("IsTitleVisible");
+            }
+        }
+
+        public string SelectedDoughnutInnerRadiusRatioString
+        {
+            get
+            {
+                return String.Format("{0:P1}.", SelectedDoughnutInnerRadiusRatio);
+            }
+        }
+
+        /*
+         *  Get/Set of layout elements
+         * */
+        public string Foreground
+        {
+            get
+            {
+                if (darkLayout)
+                {
+                    return "#FFEEEEEE";
+                }
+                if (tuIlemauLayout)
+                {
+                    return "#FFEEEEEE";
+                }
+                return "#FF666666";
+            }
+        }
+        public string MainForeground
+        {
+            get
+            {
+                if (darkLayout)
+                {
+                    return "#FFFFFFFF";
+                }
+                if (tuIlemauLayout)
+                {
+                    return "#FFFFFFFF";
+                }
+                return "#FF666666";
+            }
+        }
+        public string Background
+        {
+            get
+            {
+                if (darkLayout)
+                {
+                    return "#FF333333";
+                }
+                if (tuIlemauLayout)
+                {
+                    return "#FF333333";
+                }
+                return "#FFF9F9F9";
+            }
+        }
+        public string MainBackground
+        {
+            get
+            {
+                if (darkLayout)
+                {
+                    return "#FF000000";
+                }
+                if (tuIlemauLayout)
+                {
+                    return "#FF000000";
+                }
+                return "#FFEFEFEF";
+            }
+        }
+
+        /*
+         *  Layouts
+         * */
+        private bool darkLayout = false;
+        public bool DarkLayout
+        {
+            get
+            {
+                return darkLayout;
+            }
+            set
+            {
+                darkLayout = value;
+                NotifyPropertyChanged("DarkLayout");
+                NotifyPropertyChanged("Foreground");
+                NotifyPropertyChanged("Background");
+                NotifyPropertyChanged("MainBackground");
+                NotifyPropertyChanged("MainForeground");
+            }
+        }
+
+        private bool tuIlemauLayout = false;
+        public bool TUIlmenauLayout
+        {
+            get
+            {
+                return tuIlemauLayout;
+            }
+            set
+            {
+                tuIlemauLayout = value;
+                NotifyPropertyChanged("DarkLayout");
+                NotifyPropertyChanged("Foreground");
+                NotifyPropertyChanged("Background");
+                NotifyPropertyChanged("MainBackground");
+                NotifyPropertyChanged("MainForeground");
+            }
+        }
+
+        /*
+         *  Events
+         * */
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /*
+         *  Private methods
+         * */
+        private void NotifyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        private void LoadPalettes()
+        {
+            Palettes = new Dictionary<string, De.TorstenMandelkow.MetroChart.ResourceDictionaryCollection>();
+            Palettes.Add("Default", null);
+
+            var resources = Application.Current.Resources.MergedDictionaries.ToList();
+            foreach (var dict in resources)
+            {
+                foreach (var objkey in dict.Keys)
+                {
+                    if (dict[objkey] is De.TorstenMandelkow.MetroChart.ResourceDictionaryCollection)
+                    {
+                        Palettes.Add(objkey.ToString(), dict[objkey] as De.TorstenMandelkow.MetroChart.ResourceDictionaryCollection);
+                    }
+                }
+            }
+
+            SelectedPalette = Palettes.FirstOrDefault();
+        }
+
+        int newSeriesCounter = 1;
+        private void AddSeries()
+        {
+            ObservableCollection<TestClass> data = new ObservableCollection<TestClass>();
+
+            data.Add(new TestClass() { Category = "Globalization", Number = 5 });
+            data.Add(new TestClass() { Category = "Features", Number = 10 });
+            data.Add(new TestClass() { Category = "ContentTypes", Number = 15 });
+            data.Add(new TestClass() { Category = "Correctness", Number = 20 });
+            data.Add(new TestClass() { Category = "Naming", Number = 15 });
+            data.Add(new TestClass() { Category = "Best Practices", Number = 10 });
+
+            Series.Add(new SeriesData() { SeriesDisplayName = "New Series " + newSeriesCounter.ToString(), Items = data });
+
+            newSeriesCounter++;
+        }
+
+        /*
+         *  Constructor
+         * */
+        public GUIViewModel()
+        {
+            LoadPalettes();
+
+            AddSeriesCommand = new DelegateCommand(x => AddSeries());
+
+            ViewTypes = new ObservableCollection<string>();
+            ViewTypes.Add("Map");
+            ViewTypes.Add("Statistics");
+            ViewTypes.Add("Passwords");
+            SelectedChartType = ViewTypes.FirstOrDefault();
+
+            FontSizes = new List<double>();
+            FontSizes.Add(8.0);
+            FontSizes.Add(9.0);
+            FontSizes.Add(10.0);
+            FontSizes.Add(11.0);
+            FontSizes.Add(12.0);
+            FontSizes.Add(13.0);
+            FontSizes.Add(18.0);
+            SelectedFontSize = 12.0;
+
+            DoughnutInnerRadiusRatios = new List<double>();
+            DoughnutInnerRadiusRatios.Add(0.90);
+            DoughnutInnerRadiusRatios.Add(0.75);
+            DoughnutInnerRadiusRatios.Add(0.5);
+            DoughnutInnerRadiusRatios.Add(0.25);
+            DoughnutInnerRadiusRatios.Add(0.1);
+            SelectedDoughnutInnerRadiusRatio = 0.75;
+
+            SelectionBrushes = new List<string>();
+            SelectionBrushes.Add("Orange");
+            SelectionBrushes.Add("Red");
+            SelectionBrushes.Add("Yellow");
+            SelectionBrushes.Add("Blue");
+            SelectionBrushes.Add("[NoColor]");
+            SelectedBrush = SelectionBrushes.FirstOrDefault();
+
+            Series = new ObservableCollection<SeriesData>();
+
+            Errors = new ObservableCollection<TestClass>();
+            Warnings = new ObservableCollection<TestClass>();
+
+            ObservableCollection<TestClass> Infos = new ObservableCollection<TestClass>();
+
+            Errors.Add(new TestClass() { Category = "Globalization", Number = 75 });
+            Errors.Add(new TestClass() { Category = "Features", Number = 2 });
+            Errors.Add(new TestClass() { Category = "ContentTypes", Number = 12 });
+            Errors.Add(new TestClass() { Category = "Correctness", Number = 83});
+            //Errors.Add(new TestClass() { Category = "Naming", Number = 80 });
+            Errors.Add(new TestClass() { Category = "Best Practices", Number = 29 });
+
+            Warnings.Add(new TestClass() { Category = "Globalization", Number = 34 });
+            Warnings.Add(new TestClass() { Category = "Features", Number = 23 });
+            Warnings.Add(new TestClass() { Category = "ContentTypes", Number = 15 });
+            Warnings.Add(new TestClass() { Category = "Correctness", Number = 66 });
+            Warnings.Add(new TestClass() { Category = "Naming", Number = 56 });
+            Warnings.Add(new TestClass() { Category = "Best Practices", Number = 34 });
+
+            Infos.Add(new TestClass() { Category = "Globalization", Number = 14 });
+            Infos.Add(new TestClass() { Category = "Features", Number = 3 });
+            Infos.Add(new TestClass() { Category = "ContentTypes", Number = 55 });
+            Infos.Add(new TestClass() { Category = "Correctness", Number = 26 });
+            Infos.Add(new TestClass() { Category = "Naming", Number = 3 });
+            Infos.Add(new TestClass() { Category = "Best Practices", Number = 8 });
+
+            Series.Add(new SeriesData() { SeriesDisplayName = "Errors", Items = Errors });
+            Series.Add(new SeriesData() { SeriesDisplayName = "Warnings", Items = Warnings });
+            Series.Add(new SeriesData() { SeriesDisplayName = "Info", Items = Infos });
+        }
+
+        public ObservableCollection<SeriesData> Series
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<TestClass> Errors
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<TestClass> Warnings
+        {
+            get;
+            set;
+        }
+    }
+
+
+    public class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<object> execute,
+                       Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+
+
+    public class SeriesData
+    {
+        public string SeriesDisplayName { get; set; }
+
+        public string SeriesDescription { get; set; }
+
+        public ObservableCollection<TestClass> Items { get; set; }
+    }
+
+
+    public class TestClass : INotifyPropertyChanged
+    {
+        public string Category { get; set; }
+
+        private float _number = 0;
+        public float Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                _number = value;
+                if (PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Number"));
+                }
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
