d27800667cf78e0460498bbe0fb29e711d4ba61c
Clemens Fischer <clemens.fischer@tu-ilmenau.de>
2015-06-02 15:18:54 +0200

    Added NotifyIcon as resource

--- Softwareprojekt2015/App.xaml.cs
+++ Softwareprojekt2015/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
