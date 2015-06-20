4c5bd0c8191c5a9916020cb94847648b1407c4f2
Clemens Fischer <clemens.fischer@tu-ilmenau.de>
2015-06-20 19:40:09 +0200

    Minor changes
    Fixed language file and app title

--- Softwareprojekt2015/Properties/AssemblyInfo.cs
+++ Softwareprojekt2015/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("Softwareprojekt2015")]
+[assembly: AssemblyTitle("NSA 4 Dummies")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Softwareprojekt2015")]
+[assembly: AssemblyProduct("NSA 4 Dummies")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
